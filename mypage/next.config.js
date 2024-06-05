db93ad0ef2a5cd1aa68cb97836190d49427f1572
Hocheol <didifia9850@naver.com>
2024-06-05 20:11:47 +0900

    .

--- mypage/next.config.js
+++ mypage/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: '/hocheol18.github.io/mypage',
-    basePath: '/hocheol18.github.io/mypage',
+    assetPrefix: '/hocheol18.github.io/mypage/',
+    basePath: '/hocheol18.github.io/mypage/',
   };
   
   module.exports = nextConfig;
